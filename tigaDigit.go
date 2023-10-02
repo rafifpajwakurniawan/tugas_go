00a43b17114162171c7b922fc37cdd0113da3206
rafifpajwakurniawan <146701850+rafifpajwakurniawan@users.noreply.github.com>
2023-10-02 13:36:43 +0700

    Update tigaDigit.go

--- tigaDigit.go
+++ tigaDigit.go
@@ -4,16 +4,11 @@ import "fmt"
 
 func main() {
 	var x, d1, d2, d3, hb int
-	// note : hb itu buat nyimpan hasil bagi-nya
 	fmt.Scanln(&x)
 	d1 = x / 100
-	// 546/100 = 5,46 = 5
 	hb = x % 100
-	// 546 % 100 = 46
 	d2 = hb / 10
-	// 46/10 = 4.6 = 4
 	d3 = hb % 10
-	// 46 % 10 = 6
 
 	fmt.Println(d1, d2, d3)
 }
